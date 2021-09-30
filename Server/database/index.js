c77edc220fc443aa389a6338d46d84dd15b5c41e
Iris Huang <yilu.huang@gmail.com>
2021-09-29 21:47:45 -0700

    Added server/db query for getAll and updated command for db

--- Server/database/index.js
+++ Server/database/index.js
@@ -8,9 +8,32 @@ const pool = new Pool({
   port: 5432,
 });
 
-pool.query('SELECT * FROM products WHERE product_id = 1', (err, res) => {
-  console.log('here', err, res);
-});
+var getAll = (cb) => {
+  pool.query('SELECT * FROM products WHERE id BETWEEN 1 AND 25', (err, res) => {
+    if (err) {
+      cb(err, null);
+    } else {
+      cb(null, res.rows);
+    }
+  });
+};
 
+var getOne = (data) => {
+  pool.query('SELECT * FROM products WHERE product_id = 1', (err, res) => {
+    console.log('here', err, res);
+  });
+};
 
-module.exports = pool;
+var getStyles = (data) => {
+  pool.query('SELECT * FROM products WHERE product_id = 1', (err, res) => {
+    console.log('here', err, res);
+  });
+};
+
+var getRelated = (data) => {
+  pool.query('SELECT * FROM products WHERE product_id = 1', (err, res) => {
+    console.log('here', err, res);
+  });
+};
+
+module.exports = { pool, getAll, getOne, getRelated, getStyles };
--- Server/index.js
+++ Server/index.js
@@ -7,14 +7,29 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/products', (req, res) => {
-  res.send({message: 'Hello From Server!'});
+  db.getAll((err, data) => {
+    if (err) {
+      console.log('db.getAll err:', err);
+    } else {
+      res.send(data).status(200);
+    }
+  });
 });
+
+// app.get('/products/:product_id', (req, res) => {
+//   res.send({message: 'Hello From Server!'});
+// });
+
+// app.get('/products/:product_id/styles', (req, res) => {
+//   res.send({message: 'Hello From Server!'});
+// });
+
+// app.get('/products/:product_id/related', (req, res) => {
+//   res.send({message: 'Hello From Server!'});
+// });
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
 });
-
-
-
 
 module.exports = app;
