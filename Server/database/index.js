0afa76bff61586478e3be2d4d77a557eeee0f3b8
Iris Huang <yilu.huang@gmail.com>
2021-10-01 12:40:42 -0700

    improve speed

--- Server/database/index.js
+++ Server/database/index.js
@@ -28,7 +28,13 @@ var getOne = (data, cb) => {
 
 var getStyles = (data, cb) => {
   var final = {'product_id': data, results: null};
-  pool.query(`SELECT s.style_id, s.name, s.original_price, s.sale_price, s."default?", array_agg(distinct jsonb_build_object('thumbnail_url', p.thumbnail_url, 'url', p.url)) as photos, json_object_agg(sk.id, json_build_object('quantity', sk.quantity, 'size', sk.size)) as skus FROM styles AS s INNER JOIN photos AS p ON p.style_id = s.style_id INNER JOIN skus AS sk ON sk.style_id = s.style_id WHERE s.product_id = $1 GROUP BY s.style_id`, [data])
+  pool.query(`
+    SELECT a.style_id, a.name, a.original_price, a.sale_price, a."default?", array_agg(distinct jsonb_build_object('thumbnail_url', a.thumbnail_url, 'url', a.url)) as photos, json_object_agg(a.id, json_build_object('quantity', a.quantity, 'size', a.size)) as skus
+    FROM (SELECT s.style_id, s.name, s.original_price, s.sale_price, s."default?", p.thumbnail_url, p.url, sk.id, sk.quantity, sk.size FROM styles AS s
+    INNER JOIN (SELECT photos.style_id, photos.thumbnail_url, photos.url FROM photos) AS p ON p.style_id = s.style_id
+    INNER JOIN (SELECT skus.style_id, skus.id, skus.quantity, skus.size FROM skus) AS sk ON sk.style_id = s.style_id
+    WHERE s.product_id = $1 GROUP BY s.style_id, p.thumbnail_url, p.url, sk.id, sk.quantity, sk.size) AS a
+    GROUP BY a.style_id, a.name, a.original_price, a.sale_price, a."default?"`, [data])
     .then(res => {
       final.results = res.rows;
       cb(null, final);
