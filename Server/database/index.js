5614cd221b2207c44dc1379ffe75e22cbef94265
Iris Huang <yilu.huang@gmail.com>
2021-10-07 10:29:44 -0700

    updated query on SQL

--- Server/database/index.js
+++ Server/database/index.js
@@ -17,11 +17,14 @@ var getAll = (cb) => {
 
 var getOne = (data, cb) => {
   var result;
-  pool.query(`SELECT p.*, COALESCE (array_agg(json_build_object('value', fMisc.value, 'feature', fMisc.feature)) FILTER (WHERE fMisc.value IS NOT NULL), '{}') as features
-    FROM products AS p
-    LEFT JOIN (SELECT features.product_id, features.feature, features.value FROM features) as fMisc ON p.product_id = fMisc.product_id
-    WHERE p.product_id = $1
-    GROUP BY p.product_id`, [data])
+  pool.query(`SELECT a.*, COALESCE (array_agg(json_build_object('value', a.value, 'feature', a.feature)) FILTER (WHERE a.value IS NOT NULL), '{}') as features
+    FROM (
+      SELECT p.*, f.product_id, f.feature, f.value
+      FROM products as p
+      LEFT JOIN features as f ON p.product_id = f.product_id
+      WHERE p.product_id = $1
+    ) as a
+    GROUP BY a.product_id`, [data])
     .then(res => {
       return (
         cb(null, res.rows[0])
@@ -33,16 +36,17 @@ var getOne = (data, cb) => {
 var getStyles = (data, cb) => {
   var final = {'product_id': data, results: null};
   pool.query(`
-    SELECT s.style_id, s.name, s.original_price, s.sale_price, s."default?",
-      COALESCE (array_agg(jsonb_build_object('url', pLink.url, 'thumbnail_url', pLink.thumbnail_url)) FILTER (WHERE pLink.url IS NOT NULL), '{}') as photos,
-      COALESCE (jsonb_object_agg(sOthers.id, jsonb_build_object('size', sOthers.size, 'quantity', sOthers.quantity)) FILTER (WHERE sOthers.id IS NOT NULL), '[]') as skus
-    FROM styles AS s
-    LEFT JOIN (SELECT photos.style_id, photos.url, photos.thumbnail_url FROM photos) as pLink
-      ON pLink.style_id = s.style_id
-    LEFT JOIN (SELECT skus.id, skus.style_id, skus.size, skus.quantity FROM skus) as sOthers
-      ON sOthers.style_id = s.style_id
-    WHERE s.product_id = $1
-    GROUP BY s.style_id`, [data])
+    SELECT c.style_id, c.name, c.original_price, c.sale_price, c."default?",
+      COALESCE (array_agg(jsonb_build_object('url', c.url, 'thumbnail_url', c.thumbnail_url)) FILTER (WHERE c.url IS NOT NULL), '{}') as photos,
+      COALESCE (jsonb_object_agg(c.id, jsonb_build_object('size', c.size, 'quantity', c.quantity)) FILTER (WHERE c.id IS NOT NULL), '[]') as skus
+    FROM (
+      SELECT s.style_id, s.name, s.original_price, s.sale_price, s."default?", p.url, p.thumbnail_url, sk.id, sk.size, sk.quantity
+      FROM styles as s
+      LEFT JOIN photos as p ON p.style_id = s.style_id
+      LEFT JOIN skus as sk ON sk.style_id = s.style_id
+      WHERE s.product_id = $1
+    ) as c
+    GROUP BY c.style_id, c.name, c.original_price, c.sale_price, c."default?"`, [data])
     .then(res => {
       final.results = res.rows;
       cb(null, final);
@@ -65,3 +69,26 @@ var getRelated = (data, cb) => {
 };
 
 module.exports = { pool, getAll, getOne, getRelated, getStyles };
+
+
+// SELECT s.style_id, s.name, s.original_price, s.sale_price, s."default?",
+//       COALESCE (array_agg(jsonb_build_object('url', pLink.url, 'thumbnail_url', pLink.thumbnail_url)) FILTER (WHERE pLink.url IS NOT NULL), '{}') as photos,
+//       COALESCE (jsonb_object_agg(sOthers.id, jsonb_build_object('size', sOthers.size, 'quantity', sOthers.quantity)) FILTER (WHERE sOthers.id IS NOT NULL), '[]') as skus
+//     FROM styles AS s
+//     LEFT JOIN (SELECT photos.style_id, photos.url, photos.thumbnail_url FROM photos) as pLink
+//       ON pLink.style_id = s.style_id
+//     LEFT JOIN (SELECT skus.id, skus.style_id, skus.size, skus.quantity FROM skus) as sOthers
+//       ON sOthers.style_id = s.style_id
+//     WHERE s.product_id = $1
+//     GROUP BY s.style_id
+
+// SELECT p.*, COALESCE (array_agg(json_build_object('value', fMisc.value, 'feature', fMisc.feature)) FILTER (WHERE fMisc.value IS NOT NULL), '{}') as features
+//     FROM (
+//       SELECT p.*, f.product_id, f.feature, f.value
+//       FROM products as p
+//       LEFT JOIN
+//     )
+//     products AS p
+//     LEFT JOIN (SELECT features.product_id, features.feature, features.value FROM features) as fMisc ON p.product_id = fMisc.product_id
+//     WHERE p.product_id = $1
+//     GROUP BY p.product_id
